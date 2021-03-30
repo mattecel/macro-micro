d157c45e22ff4301cd4ba75da872d331d9a1e5e1
Matthew Ecelbarger <matteck123@gmail.com>
2021-03-29 20:36:32 -0400

    Folder Structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
+import "./App.scss";
+import RecipeListPage from './pages/RecipeListPage/RecipeListPage'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <BrowserRouter>
+        <Switch>
+          <Route path="/recipes" exact component={RecipeListPage} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
